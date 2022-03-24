aaf2ac3e56ac28a7c53cad221b27aceec4434a4d
Roel Heesterbeek <rheesterbeek@reacollege.nl>
2022-03-24 14:23:42 +0100

    adds theming color example

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,42 +1,50 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-            test: /\.(scss)$/,
-            use: [
-            {
-              // inject CSS to page
-              loader: 'style-loader'
-            }, {
-              // translates CSS into CommonJS modules
-              loader: 'css-loader'
-            }, {
-              // Run postcss actions
-              loader: 'postcss-loader',
-              options: {
-                // `postcssOptions` is needed for postcss 8.x;
-                // if you use postcss 7.x skip the key
-                postcssOptions: {
-                  // postcss plugins, can be exported to postcss.config.js
-                  plugins: function () {
-                    return [
-                      require('autoprefixer')
-                    ];
-                  }
+  mode: 'development',
+  entry: './index.js',
+  output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+      rules: [
+          {
+          test: /\.(scss)$/,
+          use: [
+          {
+            // inject CSS to page
+            loader: 'style-loader'
+          }, {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader'
+          }, {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
                 }
               }
-            }, {
-              // compiles Sass to CSS
-              loader: 'sass-loader'
-            }]
             }
-        ]
+          }, {
+            // compiles Sass to CSS
+            loader: 'sass-loader'
+          }]
+          }
+      ]
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      // compress: true,
+      // port: 9000,
     }
   }
